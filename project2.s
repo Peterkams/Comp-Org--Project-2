0cbaa2ca7d762ef9d10e62ad5b688fd444cff1db
PeterKams <peter.kamlewechi@bison.howard.edu>
2018-11-13 10:14:17 -0500

    Registers which are needed for loop are intialized and loaded

--- project2.s
+++ project2.s
@@ -3,6 +3,7 @@ my_string:	.space 3000
 too_long:	.asciiz "Input is too long."
 invalid_spaces:	.asciiz "Invalid base-36 number."
 empty_string_error: .asciiz "Input is empty."
+
 .text		#Assembly language instruction
 .globl main
 
@@ -10,6 +11,12 @@ main:
 	li $v0, 8
 	la $a0, my_string
 	syscall
+
+	la $t0, my_string	#loaded the address of the string
+	li $t2, 0 	#initialized i= 0
+	li $t3, 32 	#loaded space here
+	li $s0, 0	#initialized previous character to 0
+	li $t5, 0	#initialized num_of_chracters
 
 
 	
