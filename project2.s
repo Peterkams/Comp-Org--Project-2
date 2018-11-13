6d1ad4a87672302f0ca50c3c227e4d189ac3ac10
PeterKams <peter.kamlewechi@bison.howard.edu>
2018-11-13 10:11:40 -0500

    Program outputs are added

--- project2.s
+++ project2.s
@@ -1,5 +1,8 @@
 .data		#Data declaration section
-my_string:	.space 3000 
+my_string:	.space 3000
+too_long:	.asciiz "Input is too long."
+invalid_spaces:	.asciiz "Invalid base-36 number."
+empty_string_error: .asciiz "Input is empty."
 .text		#Assembly language instruction
 .globl main
 
